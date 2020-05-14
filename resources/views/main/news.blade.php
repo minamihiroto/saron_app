5780a2e74a9235f32975fc11da0b096e2a4f32cd
minamihiroto <kokowatamf@gmail.com>
2020-05-14 16:48:57 +0900

    スペースを揃えた

--- resources/views/main/news.blade.php
+++ resources/views/main/news.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.main')
 @section('title','News')
 @section('content')
-              {{ $item->news_title }}
-              {{ $item->news_message }}
-              <form action="/admin/news_destroy/{{ $item->id }}" method="POST">
-                {{ csrf_field() }}
-                <input type="hidden" name="_method" value="DELETE">
-                <input type="submit" class="delete" value="削除">
-              </form>
+  {{ $item->news_title }}
+  {{ $item->news_message }}
+  <form action="/admin/news_destroy/{{ $item->id }}" method="POST">
+    {{ csrf_field() }}
+    <input type="hidden" name="_method" value="DELETE">
+    <input type="submit" class="delete" value="削除">
+  </form>
 @endsection 
