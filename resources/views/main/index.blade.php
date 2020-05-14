85ea0104e35c3b527b9fed165ccf11c720c9093c
minamihiroto <kokowatamf@gmail.com>
2020-05-14 14:36:22 +0900

    news削除項目を削除

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -12,11 +12,6 @@
       @foreach($items as $item)
           <div class="alert alert-primary" role="alert">
               <a href="/main/news">{{ $item->news_title }}</a>
-              <form action="/admin/news_destroy/{{ $item->id }}" method="POST">
-                {{ csrf_field() }}
-                <input type="hidden" name="_method" value="DELETE">
-                <input type="submit" class="delete" value="削除">
-              </form>
           </div>
       @endforeach
     @else
