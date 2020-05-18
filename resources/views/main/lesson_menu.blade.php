3b4fc441781555686e4a5fe14eec862a2829f7be
minamihiroto <kokowatamf@gmail.com>
2020-05-18 11:03:41 +0900

    menuページのレイアウト更新

--- resources/views/main/lesson_menu.blade.php
+++ resources/views/main/lesson_menu.blade.php
@@ -4,37 +4,37 @@
 @section('content')
 
 <div class="menu">
-  <section class="drink-box">
+  <section class="drink-box box">
   <h1>Menu → You can take out</h1>
-      <p><span class="mgr-430">Espresso</span>450 yen</p>
-      <P><span class="mgr-421">Macchiato</span>480 yen</P>
-      <p><span class="mgr-335">Americano (hot/iced)</span>450 yen</p>
-      <p><span class="mgr-410">Cappuccino</span>500 yen</p>
-      <p><span class="mgr-345">Cafe latte (hot/iced)</span>520 yen</p>    
-      <p><span class="mgr-370">Mocha (hot/iced)</span>600 yen</p>
-      <p><span class="mgr-459">Blend</span>+100 yen</p>
-      <P><span class="mgr-295">Tall size (expect Espresso)</span>+100 yen</P>
-      <p><span class="mgr-280">Short size (expect Espresso)</span>-100 yen</p>
+      <div class="products"><p class="product">Espresso</p><p class="price">450 yen</p></div>
+      <div class="products"><p class="product">Macchiato</p><p class="price">480 yen</P></div>
+      <div class="products"><p class="product">Americano (hot/iced)</p><p class="price">450 yen</p></div>
+      <div class="products"><p class="product">Cappuccino</p><p class="price">500 yen</p></div>
+      <div class="products"><p class="product">Cafe latte (hot/iced)</p><p class="price">520 yen</p></div> 
+      <div class="products"><p class="product">Mocha (hot/iced)</p><p class="price">600 yen</p></div>
+      <div class="products"><p class="product">Blend</p><p class="price">+100 yen</p></div>
+      <div class="products"><p class="product">Tall size (expect Espresso)</p><p class="price">+100 yen</P></div>
+      <div class="products"><p class="product">Short size (expect Espresso)</p><p class="price">-100 yen</p></div>
   </section>
 
-  <section class="food-box">   
+  <section class="food-box box">   
     <h1>Side food</h1>
-    <p><span class="mgr-500">Italian waffle</span> 280 yen</p>
-    <p><span class="mgr-500">Apple souffle</span>350 yen</p>
-    <p><span class="mgr-438">Coconut pound cake</span>350 yen</p>
-    <p><span class="mgr-480">Original granola</span>600 yen</p>
-    <p><span class="mgr-464">8 Original cookies</span>200 yen each</p>
-    <p><span class="mgr-478">3 Original donut</span>200 yen each</p>
+    <div class="products"><p class="product">Italian waffle</p><p class="price">280 yen</p></div>
+    <div class="products"><p class="product">Apple souffle</p><p class="price">350 yen</p></div>
+    <div class="products"><p class="product">Coconut pound cake</p><p class="price">350 yen</p></div>
+    <div class="products"><p class="product">Original granola</p><p class="price">600 yen</p></div>
+    <div class="products"><p class="product">8 Original cookies</p><p class="price">200 yen each</p></div>
+    <div class="products"><p class="product">3 Original donut</p><p class="price">200 yen each</p></div>
   </section>
 
-  <section class="tea-box"> 
+  <section class="tea-box box"> 
     <h1>Japanease tea</h1>
     <div class="tea-text">
       <a>当店では京都宇治で栽培される”さみどり”と呼ばれる品種の茶葉を直送で送ってもらっています。”さみどり”は宇治でしか栽培されておらず、最も高級品だとされています。その”さみどり”を皆様に賞味して頂けるように、抹茶ベースで提供します。この京都の地で日本の豊さを味わってみては？</a>
     </div>
-      <p><span class="mgr-350">Green tea mocha (hot/iced)</span>650 yen</p>
-      <p><span class="mgr-355">Green tea frappuccino</span>650 yen</p>
-      <p><span class="mgr-380">Green tea (Organic)</span>450 yen</p>
+      <div class="products"><p class="product">Green tea mocha (hot/iced)</p><p class="price">650 yen</p></div>
+      <div class="products"><p class="product">Green tea frappuccino</p><p class="price">650 yen</p></div>
+      <div class="products"><p class="product">Green tea (Organic)</p><p class="price">450 yen</p></div>
   </section>
 </div>
 @endsection
