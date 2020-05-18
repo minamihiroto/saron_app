de1483721599049baebd078389463217efba40ac
minamihiroto <kokowatamf@gmail.com>
2020-05-18 17:05:26 +0900

    タグを変更

--- resources/views/member_limited/video_list.blade.php
+++ resources/views/member_limited/video_list.blade.php
@@ -1,28 +1,22 @@
-<x-head />
+@extends('layouts.member')
+@section('css','login-nav.css')
+@section('css2','style.css')
 @section('title','Videos')
-<header>
-  <a href="/">index</a>
-  <a href="/member_limited/profile">{{$user->name}}</a>
-  <a href="/member_limited">videos</a>
-  <a href="/admin/share">share</a>
-  <a href="/admin/news_share">announce</a>
-</header>
 
+  <h1>videos</h1>
   @if(count($items) > 0)
         @foreach($items as $item)
             <div class="alert alert-primary" role="alert">
-                <h1>{{ $item->video_file }}</h1>
-                <h1>{{ $item->video_title }}</h1>
-                <h1>{{ $item->video_summary }}</h1>
-                <form action="/admin/destroy/{{ $item->id }}" method="POST">
+                <div class="video">{{ $item->video_file }}</div>
+                <h2>{{ $item->video_title }}</h1>
+                <p>{{ $item->video_summary }}</p>
+                <form action="/admin/destroy/{{ $item->id }}" method="POST" class="delete">
                   {{ csrf_field() }}
                   <input type="hidden" name="_method" value="DELETE">
-                  <input type="submit" class="delete" value="削除">
+                  <input type="submit" value="削除">
                 </form>
             </div>
         @endforeach
     @else
         <div>投稿記事がありません</div>
     @endif
-</body>
-</html>
