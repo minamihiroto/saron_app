71a46b5005f42a08fabc8102e31f0ff828c060af
minamihiroto <kokowatamf@gmail.com>
2020-05-19 00:42:14 +0900

    aboutページの作成

--- resources/views/main/about.blade.php
+++ resources/views/main/about.blade.php
@@ -3,21 +3,45 @@
 @section('css','style.css')
 @section('css2','nav.css')
 @section('content')
-  <section class="introduction">
-    <img class="personimage" src="/images/sample4.jpg" alt="person-image" width="700px"> 
-    person-textperson-textperson-textperson-textperson-textperson-textperson-textperson-text
-    <img class="roomimage" src="/images/sample5.jpg" alt="room-image" width="700px">
-    room-textroom-textroom-textroom-textroom-textroom-textroom-textroom-textroom-textroom-textroom-textroom-textroom-text
+  <section class="founding">
+    <div class="master-image">
+      <h1><span class="under">Our story</span></h1>
+      <img src ="/images/master.png" height="auto" width="950px">
+      <p class="position">Master</p>
+      <p class="name">Kei Shindo</p>
+      <p class="position-two">Owner</p>
+      <p class="name-two">Shiba Takuma</p>  
+    </div>
+    <div class="story">
+      <h2>History</h2>
+      <p>私、司馬拓篤（写真左）はエスペラントという世界語の活動家である両親と、商社を経営する父親のもとで世界を
+      旅しながら育てられました。世界の様々な文化、人々の生活、富と貧困、幸せな人とそうでない人。そんな人々を
+      見ながら<br>
+      「自分って何者なんだろう？」　「どんな人生をおくりたいのかな？」<br>
+      そんな幼少期をぼんやりと過ごしてきました。<br>
+      高校を卒業して、カルフォルニアに渡りました。毎晩、目の前に夕陽が落ちていくベニスビーチのアパートメント
+      に住み、ビーチカルチャーとヒッピーカルチャーにどっぷり浸りました。<br>
+      社会人になって貿易の仕事をはじめ、海外出張の連続。世界各国のビジネスオーナーの働き方や家庭生活を間近で
+      見ながら<br>
+      「自分は何者か？どんな人生をおくりたいのか？」<br>
+      そんな自問自答の中辿り着いた目標が<br>
+      地に足がついたシンプルな生活<br>
+      衣食住<br>
+      子供と世界を旅し、子供により高い土台を用意する<br>
+      そんな生活を彩る日々のコーヒー<br>
+      皆様に最高のコーヒーをお出しするために、ハワイにコーヒー農園を購入し、生豆商社をはじめ、国産焙煎機の輸出
+      元や、世界最高峰のエスプレッソマシンの代理店となり、メイドインジャパンのコーヒーブランドに相応しい、京都
+      という土地でコーヒーロースターをオープンしました。<br>
+      我々の夢はこのブランドを京都から世界に展開することです。各国に拠点を作り、我々のコーヒーを提供すると共に
+      、若いバリスタが見聞を増やし、人生の目標を設定する機会を作れればこれ以上の幸せはありません。
+      </p>
+    </div>
+</section>
+
+  <section class="recruit" id="job">
+
   </section>
   <section class="box_images">
-    <img class="boximage" src="/images/sample1.jpg" alt="box-image" width="400px">
-    <img class="boximage" src="/images/sample2.jpg" alt="box-image" width="400px">
-    <img class="boximage" src="/images/sample3.jpg" alt="box-image" width="400px">
-    <img class="boximage" src="/images/sample2.jpg" alt="box-image" width="400px">
-    <img class="boximage" src="/images/sample3.jpg" alt="box-image" width="400px">
-    <img class="boximage" src="/images/sample1.jpg" alt="box-image" width="400px">
-    <img class="boximage" src="/images/sample3.jpg" alt="box-image" width="400px">
-    <img class="boximage" src="/images/sample1.jpg" alt="box-image" width="400px">
-    <img class="boximage" src="/images/sample2.jpg" alt="box-image" width="400px">
+   
   </section>
 @endsection 
