4b5844785ddbcc79fe70ffbebf641b3110d46740
minamihiroto <kokowatamf@gmail.com>
2020-05-20 00:55:02 +0900

    aboutのレイアウト更新

--- resources/views/main/about.blade.php
+++ resources/views/main/about.blade.php
@@ -38,10 +38,60 @@
     </div>
 </section>
 
-  <section class="recruit" id="job">
-
+  <section class="membership" id="job">
+    <h1><span class="under">Membership</span></h1>
+    <p class="overview">当店のスタッフはアルバイト含む社員一同、バリスタ研修の修了過程を保持し、皆様に最高のひと時を提供することを約束します。<br>
+      <br>
+      ご質問など等がありましたら、お気軽にお近くのスタッフまでお声掛けください。当店一同は皆様の疑問の種をお摘みします。<br>
+      <br>
+      メニュー以外にもご要望の挽き方等がありましたら、遠慮なく申し付けください。ご希望に添えるよう可能な限り善処します。<br>
+      <br>
+      コーヒーの美味しい召し上がり方、又、皆様の好みに併せた召し上がり方を提供します。<br>
+      <br>
+      コーヒーの苦手なお客様、又、お子様連れのお客様に対しても、大変満足のいくサービスを提供するとを約束します。
+    </p>
+    <h2>Would you like to work with us?</h2>  
+    <h3>私たちと一緒に働きませんか？</h3>
+    <img src="/images/joy-image.png" height="250" width="450">
+    <p class="example">アンドコーヒーのスタッフは業界の中でも最も親切で働き者で有能です。
+      コーヒー専門家、アーティスト、ライター、焼き菓子職人、デザイナー
+      、そしてドリーマーたちの集まりです。<br>
+       <br>
+       <br>
+       <br>
+       お問い合わせは下部のTELを参照して下さい。
   </section>
-  <section class="box_images">
-   
+  <section class="inside">
+    <h1><span class="under">Inside</span></h1>
+    <div class="inside-image">
+      <img src="/images/inside-eight.png"> 
+      <img src="/images/inside-eleven.png">
+      <img src="/images/inside-fifteen.png">
+      <img src="/images/inside-five.png">
+      <img src="/images/inside-four.png">
+      <img src="/images/inside-fourteen.png">
+      <img src="/images/inside-nine.png">
+      <img src="/images/inside-one.png">
+      <img src="/images/inside-seven.png">
+      <img src="/images/inside-six.png">
+      <img src="/images/inside-ten.png">
+      <img src="/images/inside-thirteen.png">
+      <img src="/images/inside-three.png">
+      <img src="/images/inside-twelve.png">
+      <img src="/images/inside-two.png"> 
+    </div>
+    <p class="inside-details">当店ならではの雰囲気を楽しんで頂くべく、キッチン内を除く全ての照明にはガス灯を使用しています。時間がゆっくり流れていくのを感じられます。<br>
+      <br>
+      当店の机と椅子はフィンランド製シモンズウッドのものを使用しています。ほのかに香る木の匂いとコーヒーの香りのコントラストをお楽しみ頂けます。<br>
+      <br>
+      カウンターオープンキッチンを設けており、いつでもコーヒーを煎ているところを背景として見て頂くべくことができます。<br>
+      <br>
+      店内はWiFiを設置しております。どうぞお気軽にご利用ください。<br>
+      Password  Auyifdklbb12097<br>
+      <br>
+      愛煙家にも当店をお楽しみ頂くべく喫煙席をご用意しています。どうぞご利用ください。<br>
+      <br>
+      当店をワークスペースとしてご利用頂いてるお客様のために、テーブルに置き型充電器を内臓しています。お持ちのスマートフォン、タブレット等を充電して頂けます。又、各所にコンセントを設けていますので、その他インターネット機器の充電にもご利用ください。            
+    </p>
   </section>
 @endsection 
