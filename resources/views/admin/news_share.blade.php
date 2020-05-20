bc0d930e54e8b79c46964d79429465f15bd58ba8
minamihiroto <kokowatamf@gmail.com>
2020-05-20 19:50:22 +0900

    フォームのレイアウトを変更

--- resources/views/admin/news_share.blade.php
+++ resources/views/admin/news_share.blade.php
@@ -5,20 +5,23 @@
 @section('title','ShareNews')
 
   <section class="share-news share">
+    <h1 class="share-title">announce</h1>
     <form action="/admin/news_share" method="post" enctype="multipart/form-data">
       {{ csrf_field() }}
-      <input type="hidden" name="id" value="1">
-      @if($errors->has('news_title'))
-          <div class="error_msg">{{ $errors->first('news_title') }}</div>
-      @endif
-      <input type="text" class="form" name="news_title" placeholder="タイトル" value="{{ old('news_title') }}">
+      <div class="contact-main">
+        <input type="hidden" name="id" value="1">
+        @if($errors->has('news_title'))
+            <div class="error_msg">{{ $errors->first('news_title') }}</div>
+        @endif
+        <p>news title</p>
+        <p><input type="text" class="form" name="news_title" value="{{ old('news_title') }}"></p>
 
-      @if($errors->has('news_message'))
-          <div class="error_msg">{{ $errors->first('news_message') }}</div>
-      @endif
-      <div>
-          <textarea class="form" name="news_message" placeholder="メッセージ">{{ old('news_message') }}</textarea>
+        @if($errors->has('news_message'))
+            <div class="error_msg">{{ $errors->first('news_message') }}</div>
+        @endif
+          <p>message</p>
+          <p><textarea class="form" name="news_message">{{ old('news_message') }}</textarea></p>
       </div>
-      <input type="submit" class="create" value="投  稿">
+      <input type="submit" class="create button" value="submit">
     </form>
   </section>
--- resources/views/admin/share.blade.php
+++ resources/views/admin/share.blade.php
@@ -6,24 +6,28 @@
 
   <section class="share-video share">
     <form action="/admin/share" method="post" enctype="multipart/form-data">
+      <h1 class="share-title">share video</h1>
       {{ csrf_field() }}
+
       <input type="hidden" name="id" value="1">
       @if($errors->has('video_file'))
           <div class="error_msg">{{ $errors->first('video_file') }}</div>
       @endif
-      <input type="file" class="form" name="video_file" placeholder="動画" value="{{ old('video_file') }}">
+      <input type="file" class="video-file" name="video_file" placeholder="動画" value="{{ old('video_file') }}">
 
-      @if($errors->has('video_title'))
-          <div class="error_msg">{{ $errors->first('video_title') }}</div>
-      @endif
-      <input type="text" class="form" name="video_title" placeholder="タイトル" value="{{ old('video_title') }}">
+      <div class="contact-main">
+        @if($errors->has('video_title'))
+            <div class="error_msg">{{ $errors->first('video_title') }}</div>
+        @endif
+        <p>video title</p>
+        <p><input type="text" class="form" name="video_title" value="{{ old('video_title') }}"></p>
 
-      @if($errors->has('video_summary'))
-          <div class="error_msg">{{ $errors->first('video_summary') }}</div>
-      @endif
-      <div>
-          <textarea class="form" name="video_summary" placeholder="メッセージ">{{ old('video_summary') }}</textarea>
+        @if($errors->has('video_summary'))
+            <div class="error_msg">{{ $errors->first('video_summary') }}</div>
+        @endif
+          <p>summary</p>
+          <p><textarea class="form" name="video_summary">{{ old('video_summary') }}</textarea></p>
       </div>
-      <input type="submit" class="create" value="投  稿">
+      <input type="submit" class="create button" value="submit">
     </form>
   </section>
