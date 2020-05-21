d9e7060c512d7e30b2d1adc8c66729034a8a2389
minamihiroto <kokowatamf@gmail.com>
2020-05-21 16:35:37 +0900

    accessのレスポンシブ化

--- resources/views/main/access.blade.php
+++ resources/views/main/access.blade.php
@@ -40,7 +40,7 @@
     <div class="condition"><p class="condition-title">payment</p><p class="condition-text">カード、電子マネー</p></div>
   </section>
 
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.079207228003!2d135.4955922651776!3d34.70318209059311!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xde3f06d708e205a3!2z5qKF55Sw6aeF!5e0!3m2!1sja!2sjp!4v1589726858966!5m2!1sja!2sjp" width="1440" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0" id="map"></iframe>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.079207228003!2d135.4955922651776!3d34.70318209059311!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xde3f06d708e205a3!2z5qKF55Sw6aeF!5e0!3m2!1sja!2sjp!4v1589726858966!5m2!1sja!2sjp" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0" id="map"></iframe>
   
 </div>
 @endsection 
